e2f623ff588c867982c2667cda5cf7daecf74c72
micahkepe <micahkepe@gmail.com>
2024-12-18 19:17:13 -0800

    Basic working main menu

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# 43Monkeys
+# 43 Monkeys
 
 In-development 2D pixel art rogue-like game made with Godot for COMP 460: Video
 Game Design at Rice University.
---
+++ scripts/main_menu.gd
@@ -0,0 +1,20 @@
+extends Control
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$VBoxContainer/StartButton.grab_focus()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level-0.tscn")
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
