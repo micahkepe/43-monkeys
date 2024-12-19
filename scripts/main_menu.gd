d403e4da6b8acaff116ee8c467f458abbb5cdd03
micahkepe <micahkepe@gmail.com>
2024-12-18 19:32:57 -0800

    Remove git plugin (just use terminal), document main_menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,5 +1,12 @@
 extends Control
-
+## The main menu for the game. 
+##
+## The main menu has "Start", "Settings", and "Quit" Button elements that can be 
+## navigated using the defined "ui-up" and "ui-down" mappings. The start button 
+## will change the scene to the first level. The quit button will exit the game 
+## by quiting the game tree. 
+##
+## @tutorial: https://docs.godotengine.org/en/3.0/getting_started/step_by_step/ui_main_menu.html#prepare-the-main-menu-scene
 
 
 # Called when the node enters the scene tree for the first time.
@@ -12,9 +19,12 @@ func _process(delta: float) -> void:
 	pass
 
 
+## Handles the press event for the start button. Navigates to scene "level-0".
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/level-0.tscn")
 
 
+## Handle the press event on the quit button. Quits the node tree to exit the 
+## game. 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
