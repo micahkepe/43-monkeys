b7105ed7102da725e200e8776f0cb8be42a6895c
micahkepe <micahkepe@gmail.com>
2025-01-02 16:44:07 -0800

    make labels more legible, navigate to tutorial on start

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -19,7 +19,7 @@ func _process(_delta: float) -> void:
 	pass
 
 
-## Handles the press event for the start button. Navigates to scene "level-0".
+## Handles the press event for the start button. Navigates to scene "tutorial".
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/tutorial.tscn")
 
