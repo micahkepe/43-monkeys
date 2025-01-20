078c093a755212dd775895342027b41531359921
micahkepe <micahkepe@gmail.com>
2025-01-19 19:14:43 -0600

    Navigate sound handling for menus

--- menus/main-menu/main_menu.gd
+++ menus/main-menu/main_menu.gd
@@ -5,40 +5,64 @@ extends Control
 ## navigated using the defined "ui-up" and "ui-down" mappings. The start button
 ## will change the scene to the first level. The quit button will exit the game
 ## by quiting the game tree.
-##
-## @tutorial: https://docs.godotengine.org/en/3.0/getting_started/step_by_step/ui_main_menu.html#prepare-the-main-menu-scene
 
-# The AudioStreamPlayer node that plays sound effects.
-@onready var sfx_player: AudioStreamPlayer = get_node("SFXPlayer")
 
-# The background music player for the theme
-@onready var theme_player: AudioStreamPlayer = get_node("BackgroundMusic")
+## The AudioStreamPlayer node that plays the select sound effect.
+@onready var select_sfx_player: AudioStreamPlayer = get_node("Sound/SelectSFXPlayer")
+
+## The AudioStreamPlayer node that plays the select sound effect.
+@onready var navigate_sfx_player: AudioStreamPlayer = get_node("Sound/NavigateSFXPlayer")
+
+## The background music player for the theme
+@onready var theme_player: AudioStreamPlayer = get_node("Sound/BackgroundMusic")
+
+## Whether this is the first time the main menu has focus. This is to prevent
+## playing the navigate sound effect when the scene is first loaded.
+var _is_first_focus: bool = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$VBoxContainer/StartButton.grab_focus()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
+	# Allow UI to settle before tracking focus changes
+	await get_tree().process_frame
+	_is_first_focus = false
 
 
 ## Handles the press event for the start button. Navigates to scene "tutorial".
 func _on_start_button_pressed() -> void:
 	theme_player.stop()
-	sfx_player.play()
+	select_sfx_player.play()
 	get_tree().change_scene_to_file("res://cutscenes/intro-cutscene/intro_cutscene.tscn")
 
 
 ## Handle the press event on the settings button. Currently does nothing.
 func _on_settings_button_pressed() -> void:
-	sfx_player.play()
 	get_tree().change_scene_to_file("res://menus/settings-menu/settings-menu.tscn")
+	select_sfx_player.play()
 
 
 ## Handle the press event on the quit button. Quits the node tree to exit the
 ## game.
 func _on_quit_button_pressed() -> void:
-	sfx_player.play()
+	select_sfx_player.play()
 	get_tree().quit()
+
+
+## Handle the focus entered event for the start button. Plays the navigate sound.
+func _on_start_button_focus_entered() -> void:
+	if not _is_first_focus:
+		navigate_sfx_player.play()
+
+
+## Handle the focus entered event for the settings button. Plays the navigate
+## sound.
+func _on_settings_button_focus_entered() -> void:
+	if not _is_first_focus:
+		navigate_sfx_player.play()
+
+
+## Handle the focus entered event for the quit button. Plays the navigate sound.
+func _on_quit_button_focus_entered() -> void:
+	if not _is_first_focus:
+		navigate_sfx_player.play()
--- menus/pause-menu/pause_menu.gd
+++ menus/pause-menu/pause_menu.gd
@@ -1,14 +1,29 @@
 extends Control
+## A pause menu that can be toggled to pause and resume the game.
+##
+## The pause menu has "Resume", "Main Menu", "Settings", and "Quit" Button
+## elements. The resume button will resume the game. The main menu button will
+## change the scene to the main menu. The quit button will exit the game by
+## quiting the game tree.
 
-@onready var sfx_player: AudioStreamPlayer = get_node("SFXPlayer")
+## The AudioStreamPlayer node that plays the select sound effect.
+@onready var sfx_player: AudioStreamPlayer = get_node("Sound/SelectSFXPlayer")
+
+## The AudioStreamPlayer node that plays the navigate sound effect.
+@onready var navigate_sfx_player: AudioStreamPlayer = get_node("Sound/NavigateSFXPlayer")
 
 # Paths to scenes (update these with the actual paths in your project)
 const MAIN_MENU_SCENE = "res://menus/main-menu/main-menu.tscn"
 
-# References to nodes
-var pause_menu_visible := false
+## If the pause menu is visible
+var _pause_menu_visible: bool = false
+
+## Whether this is the first time the main menu has focus. This is to prevent
+## playing the navigate sound effect when the scene is first loaded.
+var _is_first_focus: bool = true
 
 
+## Called when the node enters the scene tree for the first time.
 func _ready():
 	# Connect button signals using Callable
 	$VBoxContainer/Resume.connect("pressed", Callable(self, "_on_resume_pressed"))
@@ -20,20 +35,21 @@ func _ready():
 	hide()
 
 
-func _process(delta):
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
 	if Input.is_action_just_pressed("ui_cancel"): # Escape key is "ui_cancel" by default
 		toggle_pause_menu()
 
 
 func toggle_pause_menu() -> void:
-	if pause_menu_visible:
+	if _pause_menu_visible:
 		resume_game()
 	else:
 		pause_game()
 
 
 func pause_game() -> void:
-	pause_menu_visible = true
+	_pause_menu_visible = true
 	show()
 
 	# Pause the game
@@ -42,11 +58,16 @@ func pause_game() -> void:
 	# Focus on the resume button
 	$VBoxContainer/Resume.grab_focus()
 
+	# Allow UI to settle before tracking focus changes
+	await get_tree().process_frame
+	_is_first_focus = false
+
 
 func resume_game() -> void:
-	pause_menu_visible = false
+	_pause_menu_visible = false
 	hide()
 	get_tree().paused = false # Unpause the game
+	_is_first_focus = true # Reset first focus flag
 
 
 func _on_resume_pressed() -> void:
@@ -68,3 +89,24 @@ func _on_settings_pressed() -> void:
 func _on_quit_pressed() -> void:
 	sfx_player.play()
 	get_tree().quit()
+
+
+func _on_resume_focus_entered() -> void:
+	if not _is_first_focus:
+		navigate_sfx_player.play()
+
+
+func _on_main_menu_focus_entered() -> void:
+	if not _is_first_focus:
+		navigate_sfx_player.play()
+
+
+func _on_settings_focus_entered() -> void:
+	if not _is_first_focus:
+		navigate_sfx_player.play()
+
+
+func _on_quit_focus_entered() -> void:
+	if not _is_first_focus:
+		navigate_sfx_player.play()
+
