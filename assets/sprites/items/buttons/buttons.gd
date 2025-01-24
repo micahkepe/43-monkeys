daca39a1ccd2e7c164f568f009c6547d7833acde
klei04 <kevlei040428@gmail.com>
2025-01-24 15:45:32 -0600

    One button animation working

---
+++ assets/sprites/items/buttons/buttons.gd
@@ -0,0 +1,28 @@
+extends Area2D
+## A button that can be pressed or unpressed by stepping on or off the button
+
+@onready var _animated_sprite = $AnimatedSprite2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	_animated_sprite.play("just_unpressed")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+
+func _on_body_entered(body: Node2D) -> void:
+	print("collided")
+	_animated_sprite.play("unpressed_to_pressed")
+	_animated_sprite.play("just_pressed")
+	pass # Replace with function body.
+	
+
+func _on_body_exited(body: Node2D) -> void:
+	_animated_sprite.play("pressed_to_unpressed")
+	_animated_sprite.play("just_unpressed")
+	pass # Replace with function body.
