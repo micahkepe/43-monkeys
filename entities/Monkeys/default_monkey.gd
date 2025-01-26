6292ec3aa02de476833835c531309948de4204c8
GrantWT16 <gwt5035@rice.edu>
2025-01-26 01:26:13 -0600

    Demo bug fixes

--- entities/Monkeys/default_monkey.gd
+++ entities/Monkeys/default_monkey.gd
@@ -24,7 +24,7 @@ extends CharacterBody2D
 @export var banana_boomerang_scene: PackedScene
 @export var attack_range: float = 400.0         # Distance to throw bananas
 var attack_timer: float       # Time between throws
-@export var attack_cooldown = 1.25
+@export var attack_cooldown = 1.3
 
 
 ## Whether the monkey is currently detecting an enemy
