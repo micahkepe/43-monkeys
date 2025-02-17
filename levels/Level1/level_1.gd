da2bb641b50f0bfcfda7dd1c30abe2050241f8fc
micahkepe <micahkepe@gmail.com>
2025-02-17 17:02:24 -0600

    fix: deactivate puzzle 2 lazer once and only once

--- levels/Level1/level_1.gd
+++ levels/Level1/level_1.gd
@@ -14,6 +14,11 @@ var _button_states = {}
 ## Tracker for whether the lasers 1, 2, 3 are deactivated (first button puzzle)
 var _lasers_1_2_3_deactivated: bool = false
 
+
+## Tracker for whether the lasers 1, 2, 3 are deactivated (first button puzzle)
+var _laser_4_deactivated: bool = false
+
+
 #############
 
 func _ready() -> void:
@@ -36,6 +41,7 @@ func on_button_state_changed(is_pressed: bool, button: Node) -> void:
 	# Check if Buttons3,4,5,6 are all pressed
 	if are_buttons_3_4_5_6_pressed():
 		deactivate_laser_4()
+		_laser_4_deactivated = true
 
 # Returns true only if both Buttons1 and Buttons2 are pressed.
 func are_buttons_1_2_pressed() -> bool:
@@ -65,6 +71,10 @@ func deactivate_lasers_1_2_3() -> void:
 
 # Deactivates Laser4 when all required buttons are pressed.
 func deactivate_laser_4() -> void:
+	# you already completed this, no need to deactivate again
+	if _laser_4_deactivated:
+		return
+
 	for laser in lasers:
 		if laser.name == "Laser4":
 			laser.deactivate_laser()
