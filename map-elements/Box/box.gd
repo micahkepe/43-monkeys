a4f0d6e8b341f1b23667d0742d4147dcd3e02bbf
ZachK2026 <zk21@rice.edu>
2025-02-22 16:48:55 -0600

    change collision to prevent projectiles from passing through the player. fix(items): boxes should block projectiles #18
    Need to change collision layer of projectiles to be projectiles

--- map-elements/Box/box.gd
+++ map-elements/Box/box.gd
@@ -1,15 +1,17 @@
 extends RigidBody2D
 ## A pushable box that can be moved by entities.
 
-## The animated sprite for the box sprite.
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
-## Call on tree enter. Initializes all properties.
 func _ready() -> void:
-	# Set up physics properties
-	gravity_scale = 0.0  # No gravity since we're top-down
-	linear_damp = 5.0    # High damping to prevent sliding
-	lock_rotation = true # Prevent the box from rotating
+	gravity_scale = 0.0
+	linear_damp = 5.0
+	lock_rotation = true
 	contact_monitor = true
 	max_contacts_reported = 4
 	animated_sprite.play("banana_box")
+	add_to_group("moveables")
+	print_debug("Box collision_layer: ", collision_layer, " | collision_mask: ", collision_mask)
+
+func _on_body_entered(body: Node) -> void:
+	print_debug("Box hit by: ", body.name)
