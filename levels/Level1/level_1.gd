a1c354fd0224468fda6ada420b6d32f3d8221223
micahkepe <micahkepe@gmail.com>
2025-03-26 11:54:58 -0500

    refactor(music): make music fade utility inherited by all levels

--- levels/Level1/level_1.gd
+++ levels/Level1/level_1.gd
@@ -13,9 +13,6 @@ extends "res://levels/default_level.gd"
 @onready var background_music = $BackgroundMusic
 @onready var boss_music = $BossMusic
 
-@export var fade_duration: float = 1.0  # Duration of fade in seconds
-var _current_fade_tween: Tween = null
-
 ## A dictionary to track the state of each button.
 var _button_states = {}
 
@@ -144,50 +141,4 @@ func _on_next_scene_trigger_body_entered(body: Node2D) -> void:
 func _on_boss_music_trigger_body_entered(_body: Node2D) -> void:
 	if background_music.playing and not boss_dead:
 		fade_between_tracks(background_music, boss_music)
-		
-func fade_between_tracks(from_track: AudioStreamPlayer, to_track: AudioStreamPlayer) -> void:
-	print("Starting fade from:", from_track.name, "to:", to_track.name)
-	print("Initial states - From track playing:", from_track.playing, "volume:", from_track.volume_db)
-	print("Initial states - To track playing:", to_track.playing, "volume:", to_track.volume_db)
-	
-	# Cancel any existing fade tween
-	if _current_fade_tween:
-		_current_fade_tween.kill()
-		print("Killed existing tween")
-	
-	# Store original volumes
-	var from_volume_db = from_track.volume_db
-	var to_volume_db = to_track.volume_db if to_track.volume_db > -70.0 else 0.0  # Use 0.0 as default if too quiet
-	
-	print("Original volumes - From:", from_volume_db, "To:", to_volume_db)
-	
-	# Explicitly handle the transition
-	to_track.volume_db = -80.0
-	to_track.play()
-	print("Started to_track:", to_track.name, "playing:", to_track.playing)
-	
-	# Create a new tween for fading
-	_current_fade_tween = create_tween()
-	
-	# Fade out from_track
-	_current_fade_tween.tween_property(from_track, "volume_db", -80.0, fade_duration)
-	print("Started fade out of:", from_track.name)
-	
-	# After fade out completes
-	_current_fade_tween.tween_callback(func():
-		print("Fade out complete for:", from_track.name)
-		from_track.stop()
-		from_track.volume_db = from_volume_db
-		print("Reset volume for:", from_track.name, "to:", from_volume_db)
-	)
-	
-	# Create a separate tween for fading in (don't make it parallel to avoid timing issues)
-	var fade_in_tween = create_tween()
-	fade_in_tween.tween_property(to_track, "volume_db", to_volume_db, fade_duration)
-	print("Started fade in of:", to_track.name, "to target volume:", to_volume_db)
-	
-	# After fade in completes
-	fade_in_tween.tween_callback(func():
-		print("Fade in complete for:", to_track.name)
-		print("Final states - To track playing:", to_track.playing, "volume:", to_track.volume_db)
-	)
+
--- levels/default_level.gd
+++ levels/default_level.gd
@@ -1,11 +1,17 @@
 extends Node2D
 ## Default level settings that all levels inherit.
 
+## Duration of music track fade (in seconds)
+@export var music_fade_duration: float = 1.0
+
+## Reference to the current music fade tween object.
+var _current_music_fade_tween: Tween = null
+
 ## Called when there is an input event. The input event propagates up through
 ## the node tree until a node consumes it.
 func _input(event):
 	if event.is_action_pressed("toggle_full_screen"):
-		# Toggle fullscreen mode
+		# Toggle full screen mode
 		if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 		else:
@@ -34,3 +40,52 @@ func reset_input_actions():
 	for action in actions:
 		if Input.is_action_pressed(action):
 			Input.action_release(action)
+
+
+func fade_between_tracks(from_track: AudioStreamPlayer, to_track: AudioStreamPlayer) -> void:
+	print("Starting fade from:", from_track.name, "to:", to_track.name)
+	print("Initial states - From track playing:", from_track.playing, "volume:", from_track.volume_db)
+	print("Initial states - To track playing:", to_track.playing, "volume:", to_track.volume_db)
+
+	# Cancel any existing fade tween
+	if _current_music_fade_tween:
+		_current_music_fade_tween.kill()
+		print("Killed existing tween")
+
+	# Store original volumes
+	var from_volume_db = from_track.volume_db
+	var to_volume_db = to_track.volume_db if to_track.volume_db > -70.0 else 0.0  # Use 0.0 as default if too quiet
+
+	print("Original volumes - From:", from_volume_db, "To:", to_volume_db)
+
+	# Explicitly handle the transition
+	to_track.volume_db = -80.0
+	to_track.play()
+	print("Started to_track:", to_track.name, "playing:", to_track.playing)
+
+	# Create a new tween for fading
+	_current_music_fade_tween = create_tween()
+
+	# Fade out from_track
+	_current_music_fade_tween.tween_property(from_track, "volume_db", -80.0, music_fade_duration)
+	print("Started fade out of:", from_track.name)
+
+	# After fade out completes
+	_current_music_fade_tween.tween_callback(func():
+		print("Fade out complete for:", from_track.name)
+		from_track.stop()
+		from_track.volume_db = from_volume_db
+		print("Reset volume for:", from_track.name, "to:", from_volume_db)
+	)
+
+	# Create a separate tween for fading in (don't make it parallel to avoid timing issues)
+	var fade_in_tween = create_tween()
+	fade_in_tween.tween_property(to_track, "volume_db", to_volume_db, music_fade_duration)
+	print("Started fade in of:", to_track.name, "to target volume:", to_volume_db)
+
+	# After fade in completes
+	fade_in_tween.tween_callback(func():
+		print("Fade in complete for:", to_track.name)
+		print("Final states - To track playing:", to_track.playing, "volume:", to_track.volume_db)
+		)
+
