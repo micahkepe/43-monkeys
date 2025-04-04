c32b71665de42ff05bd3b2a5b40c4eea74fe33c9
GrantWT16 <gwt5035@rice.edu>
2025-04-04 15:39:16 -0500

    Neuro Boss Goon added

---
+++ entities/boids/neuro_boss_goon/neuro_boss_goon.gd
@@ -0,0 +1,9 @@
+extends "res://entities/Boids/boid.gd"
+
+func _ready() -> void:
+	health = 8.0
+	max_health = 8.0
+	
+	_anim_sprite.play("idle")
+	
+	super._ready()
---
+++ projectiles/neuro_laser/neuro_laser.gd
@@ -0,0 +1,9 @@
+extends "res://projectiles/projectiles_script.gd"
+
+func _ready() -> void:
+	animation_name = "laser_default"
+
+	# Call the parent _ready() to run the default projectile logic.
+	super._ready()
+
+	scale = Vector2(3.5, 3.5)
