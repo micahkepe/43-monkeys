4da8c99125466fb37f5bd854d9971cd4034628e2
klei04 <kevlei040428@gmail.com>
2025-04-11 14:02:50 -0500

    Level5 dialogue

---
+++ cutscenes/Level5/level_5_cutscene.gd
@@ -0,0 +1,17 @@
+extends "res://cutscenes/cutscene.gd"
+
+## Called when the node enters the scene tree.
+func _ready():
+	super._ready()
+
+	# Load images directly as Textures
+	var frame_data: Array[CutsceneFrame] = [
+		create_frame(load("res://assets/exposition/side-profiles/evil_monkey_speaking.png"), "South Carolina\nNovember 2024"),
+		create_frame(load("res://assets/exposition/intro/frame-1.png"), "Deep beneath a rural farm...\nAlpha Genesis conducts their experiments"),
+		create_frame(load("res://assets/exposition/intro/frame-2.png"), "Their secret research pushes ethical boundaries\nNo one knows what they're really doing to us"),
+		create_frame(load("res://assets/exposition/intro/frame-3.png"), "Days pass slowly in isolation\nWatching, waiting for a chance..."),
+		create_frame(load("res://assets/exposition/intro/frame-4.png"), "Until one day...\nA careless mistake changes everything"),
+		create_frame(load("res://assets/exposition/intro/frame-5.png"), "Now is my chance for freedom\nI can't waste it...")
+	]
+
+	initialize(frame_data)
